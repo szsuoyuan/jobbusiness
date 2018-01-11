4ae1578c3985e646227b2d87790f6a8db7c4ce2e
shijiahao <shishengbinbdh@163.com>
2018-01-11 15:34:25 +0800

    自定义标签

---
+++ sy_core/src/main/java/com/sy/modules/taglib/Functions.java
@@ -0,0 +1,23 @@
+package com.sy.modules.taglib;
+
+import org.apache.commons.lang3.StringUtils;
+import com.sy.modules.utils.HtmlUtil;
+
+/**
+ * <p>User: sss
+ * <p>Date: 14-2-15
+ * <p>Version: 1.0
+ */
+public class Functions {
+
+    public static String filterHtmlTags(String param) {
+    	String str="";
+    	if(StringUtils.isNotBlank(param)) {
+    		str=HtmlUtil.delHTMLTag(param);
+    	}
+    	return str.toString();
+    }
+    
+   
+}
+
